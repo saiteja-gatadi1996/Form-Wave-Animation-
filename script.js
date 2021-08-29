ea5e06ec6eafeae5bbf5a437edd45fe98485ace6
saiteja-gatadi1996 <saitejagatadi543@gmail.com>
2021-08-29 17:50:15 +0530

    f

---
+++ script.js
@@ -0,0 +1,5 @@
+const labels=document.querySelectorAll('.form-control label')
+
+labels.forEach(item=>{
+    item.innerHTML=item.innerText.split('').map((letter,idx)=>`<span style="transition-delay:${idx*50}ms">${letter}</span>`).join('')
+})
